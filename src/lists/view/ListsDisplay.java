c57606daa35f7524013db90eabee66cc072d8c8a
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-03 12:41:39 -0600

    Added javadoc documentation to the GUI.

--- src/lists/view/ListsDisplay.java
+++ src/lists/view/ListsDisplay.java
@@ -2,13 +2,27 @@ package lists.view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Simple GUI class using JOptionPane.
+ * @author Will Pearson
+ * @version 1.0 03.10.2017
+ */
 public class ListsDisplay
 {
+	/**
+	 * Displays a popup showing the supplied String to the user.
+	 * @param textToDisplay The text to be displayed.
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
 	
+	/**
+	 * Uses a popup to display a question to a user and returns their response as a String.
+	 * @param fromQuestion The question being asked.
+	 * @return The users response to the question.
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String answer = "";
