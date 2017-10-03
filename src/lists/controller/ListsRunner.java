9c0c71c8bcb6662782ec08a039f5154723ab3c03
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-03 13:48:30 -0600

    Deleted a space.

--- src/lists/controller/ListsRunner.java
+++ src/lists/controller/ListsRunner.java
@@ -2,7 +2,7 @@ package lists.controller;
 
 public class ListsRunner
 {
-	public static void main(String [ ] args)
+	public static void main(String [] args)
 	{
 		ListsController app = new ListsController();
 		app.start();
