d7e25d11902de652ea38a284cb1a0adae05e0748
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-03 13:42:22 -0600

    Added a display for the kahoots, that give little information about the kahoots.

--- src/lists/controller/ListsController.java
+++ src/lists/controller/ListsController.java
@@ -3,21 +3,34 @@ package lists.controller;
 import java.util.List;
 import java.util.ArrayList;
 import lists.model.Kahoot;
+import lists.view.ListsDisplay;
 
 public class ListsController
 {
 	// Created a list for Kahoots
 	private List<Kahoot> myKahoots;
+	private ListsDisplay popup;
 	
 	public ListsController()
 	{
 		myKahoots = new ArrayList<Kahoot>();
+		popup = new ListsDisplay();
 	}
 	
 	public void start()
 	{
 		Kahoot myFirstKahoot = new Kahoot();
 		myKahoots.add(myFirstKahoot);
+		fillTheList();
+		showTheList();
+	}
+	
+	private void showTheList()
+	{
+		for(int index = 0; index < myKahoots.size(); index ++)
+		{
+			popup.displayText(myKahoots.get(index).toString());
+		}
 	}
 	
 	private void fillTheList()
--- src/lists/model/Kahoot.java
+++ src/lists/model/Kahoot.java
@@ -20,4 +20,11 @@ public class Kahoot
 		this.creator = creator;
 		this.questionCount = questionCount;
 	}
+	
+	public String toString()
+	{
+		String description = "This Kahoot was made by " + creator + " and has " + questionCount + " questions.";
+		
+		return description;
+	}
 }
